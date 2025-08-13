12245e612efcd61d3e140b03444f34cd1f5c6ec4
m4ck-y <macario.alvaradohdez@gmail.com>
2025-08-13 10:59:05 -0600

    CHORE (CONFIG) - TSCONFIG_APP [typescript, vite] {erasableSyntaxOnly}
    - Deshabilita 'erasableSyntaxOnly' en tsconfig.app.json para permitir la compilación correcta de enums.
    
    - Este flag no oficial bloqueaba la emisión de enums porque espera solo sintaxis eliminable,
    pero los enums generan código JS y deben emitirse para funcionar en runtime.
    
    - Con este cambio se corrige el error "This syntax is not allowed when 'erasableSyntaxOnly' is enabled"
    y se restaura la funcionalidad normal del enum en Vite + TypeScript.

--- frontend3/src/App.tsx
+++ frontend3/src/App.tsx
@@ -2,9 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import {ROUTES} from  "./routes/Router"
 
 function App() {
   const [count, setCount] = useState(0)
+
+  console.warn("ROUTE test NAME enum:", ROUTES)
 
   return (
     <>
